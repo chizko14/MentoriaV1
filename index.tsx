3c4632ccbe68ba631084cbf1491fe0f93be54c82
chizko14 <doc.jvega@gmail.com>
2025-11-18 13:11:43 -0700

    Import CSS file for styling

--- index.tsx
+++ index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ErrorBoundary } from './components/ErrorBoundary';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
